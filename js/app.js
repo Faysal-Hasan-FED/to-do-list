a48ae36069371a9670f8845360d9a591e0705b95
Faysal <frontendfaysal@gmail.com>
2021-08-22 00:31:18 +0600

    error handling

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ document.getElementById("add-button").addEventListener('click', function () {
     const listContainer = document.getElementById("item-container");
     let li = document.createElement('li');
     li.classList.add("item");
-    li.innerText = inputFieldText;
-    listContainer.appendChild(li);
+    if (inputFieldText != '') {
+        li.innerText = inputFieldText;
+        listContainer.appendChild(li);
+    }
+    else {
+        alert("Please Enter Something");
+    }
+
     inputField.value = '';
 })
